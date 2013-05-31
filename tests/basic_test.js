cc2be8c523567c1972ee297277b1f4c0352986f8
George Pantazis <george@odopod.com>
2013-05-31 10:24:22 -0700

    v0.0.2, Update tests.

--- tests/basic_test.js
+++ tests/basic_test.js
@@ -14,25 +14,11 @@ exports.test = {
 
     test.expect(5);
 
-    var output = Brutalize.generate(fixture, 1);
-
-    test.equal(output.length, 5, 'Length of basic fixture at a set depth of one.');
-
-    var output = Brutalize.generate(fixture, 2);
-
-    test.equal(output.length, 9, 'Length of basic fixture at a set depth of two.');
-
-    var output = Brutalize.generate(fixture, 3);
-
-    test.equal(output.length, 10, 'Length of basic fixture at a set depth of three.');
-
-    var output = Brutalize.generate(fixture);
-
-    test.equal(output.length, 10, 'Length of basic fixture at its maximum depth / number of endpoints.');
-
-    var output = Brutalize.generate(fixture, 100);
-
-    test.equal(output.length, 10, 'Length of basic fixture at a depth much larger than its maximum.');
+    test.equal(Brutalize.generate(fixture, 1).length, 5, 'Length of basic fixture at a set depth of one.');
+    test.equal(Brutalize.generate(fixture, 2).length, 9, 'Length of basic fixture at a set depth of two.');
+    test.equal(Brutalize.generate(fixture, 3).length, 10, 'Length of basic fixture at a set depth of three.');
+    test.equal(Brutalize.generate(fixture).length, 10, 'Length of basic fixture at its maximum depth / number of endpoints.');
+    test.equal(Brutalize.generate(fixture, 100).length, 10, 'Length of basic fixture at a depth much larger than its maximum.');
 
     test.done();
   }
